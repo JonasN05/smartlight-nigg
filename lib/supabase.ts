df8be61a011e58ebec2a3d89eed847f6a22609d8
JonasN05 <jonas.nigg@student.htldornbirn.at>
2024-03-25 22:22:04 +0100

    supabase.ts edited

---
+++ lib/supabase.ts
@@ -0,0 +1,15 @@
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://mgzarvkajohiegiyowtb.supabase.com';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1nemFydmtham9oaWVnaXlvd3RiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEyODk5ODYsImV4cCI6MjAyNjg2NTk4Nn0.dzwhcB3AG3Hd7PSYeN3X53BYdJTuVPVQQtaK5QJ9VMs';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        storage: AsyncStorage,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+});
