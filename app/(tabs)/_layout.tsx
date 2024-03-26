e3602193f13a6402df3dd9b1226b7943b0a23995
JonasN05 <jonas.nigg@student.htldornbirn.at>
2024-03-26 02:37:58 +0100

    changing layout of app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,9 +4,7 @@ import { Platform, Pressable, useColorScheme } from 'react-native';
 
 import Colors from '../../constants/Colors';
 
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
+
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
@@ -52,6 +50,21 @@ export default function TabLayout() {
           title: 'Home',
 
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          // headerRight: () => (
+          //   <Link href="/modal" asChild>
+          //     <Pressable>
+          //       {({ pressed }) => (
+          //         <FontAwesome
+          //           name="info-circle"
+          //           size={25}
+          //           color={Colors[colorScheme ?? 'light'].text}
+          //           style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+          //         />
+          //       )}
+          //     </Pressable>
+          //   </Link>
+          // ),
+
         }}
       />
       <Tabs.Screen
--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -7,8 +7,6 @@ export default function TabAcountScreen() {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Konto</Text>
-            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-            <EditScreenInfo path="app/(tabs)/two.tsx" />
         </View>
     );
 }
